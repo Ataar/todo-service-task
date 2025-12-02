7614d9175cb92172397131bee92a85f6c0f28cfc
Ataar <mateenataar07@gmail.com>
2025-12-02 23:00:20 +0530

    Re-track navbar folder

--- src/app/shared/components/navbar/navbar-toggle.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NavbarToggleDirective } from './navbar-toggle.directive';
-
-describe('NavbarToggleDirective', () => {
-  it('should create an instance', () => {
-    // const directive = new NavbarToggleDirective(navbar);
-    // expect(directive).toBeTruthy();
-  });
-});
