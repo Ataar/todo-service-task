c472a286c0b28198c6927f0c14a43d95b85c09a0
Ataar <mateenataar07@gmail.com>
2025-12-05 23:38:13 +0530

    first-commit

--- src/app/shared/components/todo/todo.component.ts
+++ src/app/shared/components/todo/todo.component.ts
@@ -4,77 +4,77 @@ import { Itodo } from 'src/app/modules/todo';
 import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-todo',
-  templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+selector: 'app-todo',
+templateUrl: './todo.component.html',
+styleUrls: ['./todo.component.scss']
 })
 export class TodoComponent implements OnInit {
-  
+
 @ViewChild('todoForm') todoForm!: NgForm;
 
-  tododArr: Itodo[] = [];
-  todoItem: string = '';
-  isEdit: boolean = false;
-  editId!: number;
+tododArr: Itodo[] = [];
+todoItem: string = '';
+isEdit: boolean = false;
+editId!: number;
 
-  constructor(private todo: TodoService) { }
+constructor(private todo: TodoService) { }
 
-  ngOnInit(): void {
-    this.todo.fetchalldata().subscribe(res => {
-      this.tododArr = res;
-    });
-  }
-
-
-
-  onAdd() {
-    if (this.todoForm.invalid) return;
-
-    const newTodo: Itodo = {
-      todoId: this.tododArr.length + 1,
-      todoItem: this.todoItem
-    };
-
-    console.log(newTodo);
-    
-    
-
-    this.todo.addTodo(newTodo).subscribe(res => {
-      this.tododArr = res;
-      this.todoForm.resetForm(); 
-    });
-  }
+ngOnInit(): void {
+this.todo.fetchalldata().subscribe(res => {
+this.tododArr = res;
+});
+}
 
 
 
-  onUpdate() {
-  const updatedTodo: Itodo = {
-    todoItem: this.todoItem,
-    todoId: this.editId
-  };
+onAdd() {
+if (this.todoForm.invalid) return;
 
-  this.todo.updateTodo(updatedTodo).subscribe(res => {
-    this.tododArr = res;
-    this.resetForm();
-  });
+const newTodo: Itodo = {
+todoId: this.tododArr.length + 1,
+todoItem: this.todoItem
+};
+
+console.log(newTodo);
+
+
+
+this.todo.addTodo(newTodo).subscribe(res => {
+this.tododArr = res;
+this.todoForm.resetForm(); 
+});
+}
+
+
+
+onUpdate() {
+const updatedTodo: Itodo = {
+todoItem: this.todoItem,
+todoId: this.editId
+};
+
+this.todo.updateTodo(updatedTodo).subscribe(res => {
+this.tododArr = res;
+this.resetForm();
+});
 }
 
 private resetForm() {
-  this.todoForm.resetForm();
-  this.todoItem = '';
-  this.isEdit = false;
+this.todoForm.resetForm();
+this.todoItem = '';
+this.isEdit = false;
 }
 
 
-  onEdit(item: Itodo) {
-    this.todoItem = item.todoItem;
-    this.editId = item.todoId;
-    this.isEdit = true;
-  }
+onEdit(item: Itodo) {
+this.todoItem = item.todoItem;
+this.editId = item.todoId;
+this.isEdit = true;
+}
 
-  onDelete(id: number) {
-    this.todo.deleteTodo(id).subscribe(res => {
-      this.tododArr = res;
-    });
-  }
+onDelete(id: number) {
+this.todo.deleteTodo(id).subscribe(res => {
+this.tododArr = res;
+});
+}
 }
